e07a1efa4bc8e0af0833b48bcce49724e55c69c7
Florian Peschka <ich@florianpeschka.de>
2018-12-04 21:39:59 +0100

    CHANGE brand, header and links are now correct with new name

--- src/app.js
+++ src/app.js
@@ -4,13 +4,16 @@ import Files from "./containers/files";
 import ReloadButton from "./containers/reloadButton";
 import TagGroups from "./containers/tagGroups";
 import Breadcrumbs from "./containers/breadcrumbs";
+import Octicon, { Tag } from "@githubprimer/octicons-react";
 
 export default () => (
     <div>
         <header>
             <Navbar dark className="bg-dark mb-3">
                 <Container>
-                    <NavbarBrand tag="a" href="/"><strong>tagster</strong></NavbarBrand>
+                    <NavbarBrand tag="a" href="/">
+                        <h1><Octicon icon={Tag} size="medium" /> tikett</h1>
+                    </NavbarBrand>
                     <ReloadButton />
                 </Container>
             </Navbar>
@@ -31,7 +34,7 @@ export default () => (
         <hr />
         <footer className="text-center mb-3">
             <Container>
-                tagster &copy; Florian Peschka
+                <a href="https://github.com/flopes89/tikett">tikett</a> &copy; Florian Peschka
             </Container>
         </footer>
     </div>
