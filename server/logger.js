4712cc8379240b48a7baf99428e7d87b30aa7c72
Florian Peschka <ich@florianpeschka.de>
2018-12-30 09:54:59 +0100

    Add padding to logger names and change timestamp format

--- server/logger.js
+++ server/logger.js
@@ -7,16 +7,26 @@ const formatter = winston.format.printf((info) => {
         metaObject = "\r\n" + JSON.stringify(info.meta, null, 2);
     }
 
-    return `${info.timestamp} ${info.level} [${info.label}] ${info.message}${metaObject}`;
+    let label = info.label;
+
+    if (label.length > 8) {
+        label = label.substring(label.length - 8);
+    } else {
+        label = label.padStart(8, " ");
+    }
+
+    return `${info.timestamp} ${info.level} ${label} ${info.message}${metaObject}`;
 });
 
 module.exports = (name) => winston.createLogger({
     level: "silly",
     format: winston.format.combine(
         winston.format.splat(),
-        winston.format.timestamp(),
-        winston.format.cli(),
+        winston.format.timestamp({
+            format: "HH:mm:ss.SSS"
+        }),
         winston.format.label({ label: name }),
+        winston.format.cli(),
         formatter,
     ),
     transports: [new winston.transports.Console({
